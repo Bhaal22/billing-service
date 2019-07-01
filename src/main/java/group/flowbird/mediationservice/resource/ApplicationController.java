7bd10a5586982a28aaf9faf06fcbbcff51243e07
yusrib <b.yusri@brickparking.com>
2019-07-01 14:13:08 +0200

    BPP-1083 : integrate git commit into application version and cleanup unused codes.

--- src/main/java/group/flowbird/mediationservice/resource/ApplicationController.java
+++ src/main/java/group/flowbird/mediationservice/resource/ApplicationController.java
@@ -1,28 +1,16 @@
 package group.flowbird.mediationservice.resource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import group.flowbird.mediationservice.dto.VersionDto;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
-import java.io.Serializable;
-import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import java.io.Serializable;
-import java.util.stream.Collectors;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.io.*;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import group.flowbird.mediationservice.*;
-
+@Slf4j
 @RestController
 @RequestMapping("/")
 @Api(value = "Application Information")
@@ -30,6 +18,41 @@ public class ApplicationController {
 
     @RequestMapping(method={RequestMethod.GET},value={"/version"})
     public String getVersion() {
-        return Application.getVersion();
+        return mapToVersion().toString();
+    }
+
+    @RequestMapping(method={RequestMethod.GET},value={"/name"})
+    public String getApplicationName() {
+        return "Mediation Service";
+    }
+
+    private String readFromInputStream() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("git.properties");
+        StringBuilder propertyBuilder = new StringBuilder();
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader br = new BufferedReader(inputStreamReader);
+            String line;
+            while ((line = br.readLine()) != null ) {
+                propertyBuilder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            log.info("git.properties resource is not found.");
+        }
+
+        return propertyBuilder.toString();
+    }
+
+    private VersionDto mapToVersion() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        VersionDto version = new VersionDto();
+        try {
+            version = objectMapper.readValue(readFromInputStream(), VersionDto.class);
+        } catch (IOException e) {
+            log.info("There is no version information available.");
+        }
+
+        return version;
     }
 }
